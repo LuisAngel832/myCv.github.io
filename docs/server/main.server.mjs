c8dd8de6ad6bccc98b883a28f05abb94a107c30f
Luis <luisgelipe.12340@gmail.com>
2025-04-22 09:51:10 -0600

    creacion de docs

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-OVQRRA3P.mjs";import"./chunk-XGHHAP73.mjs";import"./chunk-XKLCX4PC.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
