c32373d240797a0835ddac9f65b5d641bb717353
Luis <luisgelipe.12340@gmail.com>
2025-04-26 17:03:24 -0600

    resolve fixes

--- src/app/services/languages-service/languages.service.ts
+++ src/app/services/languages-service/languages.service.ts
@@ -19,25 +19,4 @@ export class LanguagesService {
     return this.LanguageRef;
   }
 } 
- import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Language } from '../../models/language.model'; 
-
-@Injectable({
-  providedIn: 'root'
-})
-export class LanguagesService {
-
-  private dbPath = '/Language';
-
-  LanguageRef: AngularFirestoreCollection<Language>;
-
-  constructor(private db: AngularFirestore) {
-    this.LanguageRef = db.collection(this.dbPath);
-  }
-
-  getLanguage(): AngularFirestoreCollection<Language> {
-    return this.LanguageRef;
-  }
-} 
  
