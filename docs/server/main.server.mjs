05e63132bfe0ee1ffdbd1f60f26a19302d4b0c2d
Luis <luisgelipe.12340@gmail.com>
2025-04-26 22:02:41 -0600

    update docs

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AI455MBU.mjs";import"./chunk-ZFPDEIY7.mjs";import"./chunk-XKLCX4PC.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
+import{a}from"./chunk-KM5WPFBX.mjs";import"./chunk-U6O4LFLW.mjs";import"./chunk-XKLCX4PC.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-EXD34EPT.mjs";export{a as default};
